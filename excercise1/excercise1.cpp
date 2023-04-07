fcea0ded10341dd9a7a77ddc546a396f911284c5
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-04-07 10:23:32 +0700

    deklarasi array vina dengan jumlah max data

---
+++ excercise1/excercise1.cpp
@@ -0,0 +1,33 @@
+//1.Algoritma sangat penting untuk menyelesaikan masalah karena algoritma memberikan cara yang terstruktur, efisien, dan efektif dalam memecahkan masalah tertentu. 
+//Dengan menggunakan algoritma, masalah dapat dipecahkan menjadi serangkaian langkah atau instruksi logis yang mudah diikuti dan dipahami oleh mesin dan manusia. 
+//Selain itu, algoritma juga dapat membantu meningkatkan kinerja sistem atau program dengan mengurangi waktu dan sumber daya yang diperlukan untuk menyelesaikan tugas tertentu. 
+//Dengan menggunakan algoritma yang tepat, suatu masalah dapat diselesaikan dengan cara yang lebih efisien dan lebih cepat, sehingga menghemat waktu dan biaya yang dibutuhkan.
+
+//2.Array: Array adalah struktur data sederhana yang terdiri dari sejumlah elemen dengan jenis data yang sama. 
+//Setiap elemen array memiliki indeks yang unik, yang digunakan untuk mengakses elemen tersebut. 
+//Operasi umum pada array adalah mengisi nilai pada elemen array, membaca nilai dari elemen array, dan memperbarui nilai elemen array.
+//Linked List: Linked list adalah struktur data yang terdiri dari serangkaian simpul yang terhubung satu sama lain. 
+//Setiap simpul memiliki dua bagian, yaitu data dan referensi ke simpul berikutnya dalam linked list. 
+//Operasi umum pada linked list adalah menambahkan simpul ke awal atau akhir linked list, menghapus simpul dari linked list, dan mengakses nilai data pada simpul tertentu.
+
+//3.Ada beberapa faktor yang dapat mempengaruhi efisiensi waktu eksekusi suatu program, antara lain:
+//Kompleksitas algoritma: menggunakan algoritma yang lebih sederhana dan efisien dapat mempercepat waktu eksekusi program.
+//Ukuran data input: Semakin besar ukuran data input, semakin lama waktu yang dibutuhkan program untuk memprosesnya.
+//Kualitas kode: Kualitas kode yang buruk, seperti penggunaan variabel yang tidak efisien atau penggunaan loop yang tidak diperlukan, dapat memperlambat waktu eksekusi program. 
+//Sistem operasi dan hardware: sistem operasi yang lebih cepat atau hardware yang lebih kuat dapat membantu meningkatkan efisiensi waktu eksekusi program
+//Bahasa pemrograman: Memilih bahasa pemrograman yang tepat untuk tugas yang diinginkan dapat membantu meningkatkan efisiensi waktu eksekusi program.
+
+//4.Untuk mengurutkan data yang ukurannya besar, algoritma yang paling efisien adalah Merge Sort. karena algoritma Merge Sort memiliki kompleksitas waktu O(n log n), yang artinya waktu yang dibutuhkan untuk mengurutkan data tumbuh secara proporsional dengan logaritma dari jumlah data. 
+//Oleh karena itu, algoritma Merge Sort lebih cepat dan lebih efisien dibandingkan dengan algoritma pengurutan lainnya seperti Bubble Sort atau Selection Sort yang memiliki kompleksitas waktu yang lebih tinggi untuk data yang ukurannya besar
+
+//5.algoritma loglinier adalah merge sort dan quick sort sedangkan algoritma quadratic adalah insertion sort, bubble sort dan selection sort
+
+#include <iostream>
+using namespace std;
+
+// Maksimal jumlah data
+const int MAX_DATA = 76;
+
+// Deklarasi array vina dengan ukuran maksimum MAX_DATA
+int vina[MAX_DATA];
+
